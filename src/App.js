ef20ec72e1d4e0691844614b49404cda4a0179b2
Casey James Perno <casey@khaosophy.com>
2022-07-22 17:33:22 -0400

    basic random recipe functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [cards, setCards] = useState(null);
+  const [randomRecipe, setRandomRecipe] = useState(null);
+  let bigMeals = useRef(null);
+  let leftovers = useRef(null);
+
+  useEffect(() => {
+    fetch(`https://api.trello.com/1/boards/ymfl7XFT/cards?key=${process.env.REACT_APP_TRELLO_KEY}&token=${process.env.REACT_APP_TRELLO_TOKEN}`)
+      .then(res => res.json())
+      .then(json => {
+        setCards(json);
+        bigMeals.current = json.filter(recipe => recipe.labels.find(label => label.name === "Big Meal"));
+        leftovers.current = json.filter(recipe => recipe.labels.find(label => label.name === "Leftovers"));
+      });
+  }, []);
+
+  function getRandomBigMeal(){
+    const randomBigMeal = bigMeals.current[Math.floor(Math.random()*bigMeals.current.length)];
+    setRandomRecipe(randomBigMeal);
+  }
+
+  function getRandomLeftovers() {
+    const randomLeftovers = leftovers.current[Math.floor(Math.random()*leftovers.current.length)];
+    setRandomRecipe(randomLeftovers);
+  }
+
+  if(!cards) return null;
+  if(!randomRecipe) {
+    return (
+      <div className="container">
+        <h1>What type of meal do you want?</h1>
+        <div className="mt-3 d-flex flex-column align-items-start">
+          <button
+            className="btn btn-primary mb-3"
+            onClick={getRandomBigMeal}
+          >
+            Random Big Meal
+          </button>
+          <button
+            className="btn btn-primary"
+            onClick={getRandomLeftovers}
+          >
+            Random Meal with Leftovers
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <h1>{randomRecipe.name}</h1>
+        <p>{randomRecipe.desc}</p> {/* todo: parse markdown */}
+
+        <div className="d-flex flex-column align-items-start">
+          <a href={randomRecipe.url} className="btn btn-secondary mb-3">See Recipe on Trello</a>
+          <button className="btn btn-primary mb-3" onClick={getRandomBigMeal}>Get a Different Big Meal</button>
+          <button className="btn btn-primary" onClick={getRandomLeftovers}>Get a Different Meal with Leftovers</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
